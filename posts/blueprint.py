9542995f4b6e79c1ff769f0f6e03a728d775730e
Vladimir Petrov <p37rov.nvkz@gmail.com>
2021-01-06 23:55:10 +0700

    Create index view function in blueprint

--- posts/blueprint.py
+++ posts/blueprint.py
@@ -1,5 +1,9 @@
 #flask
 from flask import Blueprint
+from flask import render_template
 
+posts = Blueprint('posts', __name__, template_folder='templates')
 
-posts = Blueprint('posts', __name__,template_folder='templates')
+@posts.route('/')
+def index():
+    return render_template('posts/index.html')
