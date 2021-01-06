42368ebcd152a79ada0f51e38552edc43298edc4
Vladimir Petrov <p37rov.nvkz@gmail.com>
2021-01-06 23:48:53 +0700

    Register blueprint posts with app

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
 #project
 from config import DevelopmentConfig
+from posts.blueprint import posts
 #flask
 from flask import Flask
 
 app = Flask(__name__)
 app.config.from_object(DevelopmentConfig)
+
+app.register_blueprint(posts, url_prefix='/blog')
 
 
 
