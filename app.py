ed3ac2293fbf1479aa8f541bc052aebb4fbe4d4c
Vladimir Petrov <p37rov.nvkz@gmail.com>
2021-01-06 19:33:55 +0700

    Reformat import style

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
-from flask import Flask
+#project
 from config import DevelopmentConfig
+#flask
+from flask import Flask
 
 app = Flask(__name__)
 app.config.from_object(DevelopmentConfig)
--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+#project
 from app import app
 import view
 
