c15197e2b88dfe4bf1de816f6fbdef67fe749d36
Vladimir Petrov <p37rov.nvkz@gmail.com>
2021-01-07 19:29:49 +0700

    Add database settings
    Use environment variables
    
    export VAR_NAME=value
    >>import os
    >>os.environ.get('VAR_NAME')
    'value'

--- config.py
+++ config.py
@@ -1,8 +1,18 @@
+#stdlib
+import os
+
 class DevelopmentConfig(object):
     """
     Development configurations
     """
     DEBUG = True
+
+    #DATABASE SETTINGS
+    DB_USER = os.environ.get('DB_USER')
+    DB_PASSWORD = os.environ.get('DB_PASSWORD')
+    DB_NAME = 'flask_blog_tutorial_db'
+    SQLALCHEMY_DATABASE_URI = f"mysql+mysqlconnector://{DB_USER}:{DB_PASSWORD}@localhost/{DB_NAME}"
+
     #SQLALCHEMY_ECHO = True
     #ASSETS_DEBUG = True
     #DATABASE = 'teamprojet_db'
