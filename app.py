42cfc4a08fc5dbf5895bfbf46f9db427e7e11682
Vladimir Petrov <p37rov.nvkz@gmail.com>
2021-01-07 19:51:42 +0700

    Create variable db

--- app.py
+++ app.py
@@ -3,9 +3,13 @@ from config import DevelopmentConfig
 from posts.blueprint import posts
 #flask
 from flask import Flask
+#flask_sqlalchemy
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config.from_object(DevelopmentConfig)
+
+db = SQLAlchemy(app)
 
 app.register_blueprint(posts, url_prefix='/blog')
 
