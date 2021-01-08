fe23da7c929b060d191380deda916db0339be1d2
Vladimir Petrov <p37rov.nvkz@gmail.com>
2021-01-08 18:38:55 +0700

    Fix сircular import error

--- app.py
+++ app.py
@@ -1,16 +1,15 @@
-#project
-from config import DevelopmentConfig
 #flask
 from flask import Flask
 #flask_sqlalchemy
 from flask_sqlalchemy import SQLAlchemy
+#project
+from config import DevelopmentConfig
 
 app = Flask(__name__)
 app.config.from_object(DevelopmentConfig)
 
 db = SQLAlchemy(app)
 
-app.register_blueprint(posts, url_prefix='/blog')
 
 
 
--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from app import db
 from posts.blueprint import posts
 import view
 
+app.register_blueprint(posts, url_prefix='/blog')
 
 if __name__ == '__main__':
     app.run()
