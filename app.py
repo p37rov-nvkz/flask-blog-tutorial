e2e3a6e50a4bdbb94de114d401f6ea011c9b5f92
Vladimir Petrov <p37rov.nvkz@gmail.com>
2021-01-08 19:41:19 +0700

    Create migrate instance and register migrate command

--- app.py
+++ app.py
@@ -2,6 +2,10 @@
 from flask import Flask
 #flask_sqlalchemy
 from flask_sqlalchemy import SQLAlchemy
+#flask_migrate
+from flask_migrate import Migrate, MigrateCommand
+#flask_script
+from flask_script import Manager
 #project
 from config import DevelopmentConfig
 
@@ -10,6 +14,9 @@ app.config.from_object(DevelopmentConfig)
 
 db = SQLAlchemy(app)
 
+migrate = Migrate(app, db)
+manager = Manager(app)
+manager.add_command('db', MigrateCommand)
 
 
 
