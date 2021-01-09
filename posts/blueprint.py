89ed5fa6885a3140e62eab24304fbd01a8348281
Vladimir Petrov <p37rov.nvkz@gmail.com>
2021-01-09 13:36:13 +0700

    Add view tag_detail

--- posts/blueprint.py
+++ posts/blueprint.py
@@ -3,7 +3,7 @@ from flask import Blueprint
 from flask import render_template
 
 #project
-from models import Post
+from models import Post, Tag
 
 posts = Blueprint('posts', __name__, template_folder='templates', static_folder='static')
 
@@ -12,9 +12,16 @@ def index():
     posts = Post.query.all()
     return render_template('posts/index.html', posts=posts)
 
-
+# http://localhost/blog /<slug>
 @posts.route('/<slug>')
 def post_detail(slug):
     post = Post.query.filter(Post.slug==slug).first()
     tags = post.tags
     return render_template('posts/post_detail.html', post=post, tags=tags)
+
+# http://localhost/blog/ tag/<slug>
+@posts.route('/tag/<slug>')
+def tag_detail(slug):
+    tag = Tag.query.filter(Tag.slug==slug).first()
+    posts = tag.posts.all()
+    return render_template('posts/tag_detail.html', tag=tag, posts=posts)
