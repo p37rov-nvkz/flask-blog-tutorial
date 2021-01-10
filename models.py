1fe9711f6d6dd0e90d9fe2d72132d5bc214214c2
Vladimir Petrov <p37rov.nvkz@gmail.com>
2021-01-10 17:50:27 +0700

    Edit tag.__repr__ for human

--- models.py
+++ models.py
@@ -114,5 +114,5 @@ class Tag(db.Model):
         self.slug = slugify(self.name)
 
     def __repr__(self):
-        return f'<Tag id: {self.id}, name: {self.name}>'
+        return f'{self.name}>'
 
