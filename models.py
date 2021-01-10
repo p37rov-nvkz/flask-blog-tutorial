4f8ef8d89c6c4ab9c2cbad65e22198545f9b1f4f
Vladimir Petrov <p37rov.nvkz@gmail.com>
2021-01-10 19:02:40 +0700

    Add Role models and ManyToMany tables rolse_user for Flask security

--- models.py
+++ models.py
@@ -122,8 +122,21 @@ class Tag(db.Model):
 
 # Flask security model's
 
+roles_user = db.Table('roles_users',
+                      db.Column('user_id', db.Integer(), db.ForeignKey('user.id')),
+                      db.Column('role_id', db.Integer(), db.ForeignKey('role.id')))
+
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer(), primary_key=True)
     email = db.Column(db.String(100), unique=True)
     password = db.Column(db.String(255))
     active = db.Column(db.Boolean())
+    #relationship
+    roles = db.relationship('Role', secondary=roles_user, backref=db.backref('users', lazy='dynamic'))
+
+
+class Role(db.Model, RoleMixin):
+    id = db.Column(db.Integer(), primary_key=True)
+    name = db.Column(db.Integer(100), unique=True)
+    description = db.Column(db.String(255))
+
