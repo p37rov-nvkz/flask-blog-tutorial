f959e317a42f4d9fac04bdaf84bcd51ef36c8b57
Vladimir Petrov <p37rov.nvkz@gmail.com>
2021-01-12 19:52:39 +0700

    Добавил кастомную страницу 404

--- view.py
+++ view.py
@@ -14,3 +14,7 @@ def index():
         return render_template('posts/index.html', posts=posts)
     else:
         return render_template('index.html')
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
